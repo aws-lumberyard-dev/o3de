7403684376964baee847d8a8d17dca800a356664
Jagathesan <nehjagat@amazon.com>
2021-08-31 09:39:41 -0700

    added non-copyright header issue files

--- Gems/AWSLogExplorer/cdk/aws_logexplorer/aws_logexplorer_stack.py
+++ Gems/AWSLogExplorer/cdk/aws_logexplorer/aws_logexplorer_stack.py
@@ -5,12 +5,12 @@ For complete copyright and license terms please see the LICENSE at the root of t
 SPDX-License-Identifier: Apache-2.0 OR MIT
 """
 
-from aws_cdk import core as cdk
+from aws_cdk import core
 
 
-class CdkStack(cdk.Stack):
+class AWSLogExplorerStack(core.Stack):
 
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
