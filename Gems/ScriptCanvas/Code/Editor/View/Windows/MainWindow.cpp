d7a416faab0ee2a34998f7da715a1b4f049e6025
carlitosan <82187351+carlitosan@users.noreply.github.com>
2021-11-30 17:18:27 -0800

    add pane close to system tick bus
    Signed-off-by: carlitosan <82187351+carlitosan@users.noreply.github.com>

--- Gems/ScriptCanvas/Code/Editor/View/Windows/MainWindow.cpp
+++ Gems/ScriptCanvas/Code/Editor/View/Windows/MainWindow.cpp
@@ -3937,13 +3937,13 @@ namespace ScriptCanvasEditor
 
     bool MainWindow::EvaluateSaveAttempt()
     {
-        const AZ::s64 k_saveAttemptSeconds = 20;
+        const AZ::s64 SaveAttemptSeconds = 20;
 
         if (m_saveAttemptInProgress)
         {
             auto saveDuration = AZStd::chrono::seconds(AZStd::chrono::system_clock::now() - m_saveAttemptTime).count();
 
-            if (saveDuration > k_saveAttemptSeconds)
+            if (saveDuration > SaveAttemptSeconds)
             {
                 WarnOnFailedSaveAttempt();
             }
@@ -3975,7 +3975,10 @@ namespace ScriptCanvasEditor
 
         AzFramework::AssetSystem::LaunchAssetProcessor();
         AZ::SystemTickBus::Handler::BusDisconnect();
-        AzToolsFramework::CloseViewPane(LyViewPane::ScriptCanvas);
+        AZ::SystemTickBus::QueueFunction([]()
+        {
+            AzToolsFramework::CloseViewPane(LyViewPane::ScriptCanvas);
+        });
     }
 
     void MainWindow::OnCommandStarted(AZ::Crc32)
