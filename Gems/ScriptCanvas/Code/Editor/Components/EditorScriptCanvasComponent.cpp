beb5303308a137cc08358d3f60ad299d15f70587
carlitosan <82187351+carlitosan@users.noreply.github.com>
2021-12-08 22:54:21 -0800

    small fixes for SC play in editor and warnings
    Signed-off-by: carlitosan <82187351+carlitosan@users.noreply.github.com>

--- Gems/ScriptCanvas/Code/Editor/Components/EditorScriptCanvasComponent.cpp
+++ Gems/ScriptCanvas/Code/Editor/Components/EditorScriptCanvasComponent.cpp
@@ -431,17 +431,7 @@ namespace ScriptCanvasEditor
         AzToolsFramework::ToolsApplicationNotificationBus::Broadcast(&AzToolsFramework::ToolsApplicationEvents::InvalidatePropertyDisplay, AzToolsFramework::Refresh_EntireTree_NewContent);
     }
 
-    void EditorScriptCanvasComponent::OnStartPlayInEditor()
-    {
-        ScriptCanvas::Execution::PerformanceStatisticsEBus::Broadcast(&ScriptCanvas::Execution::PerformanceStatisticsBus::ClearSnaphotStatistics);
-    }
-
-    void EditorScriptCanvasComponent::OnStopPlayInEditor()
-    {
-        AZ::ScriptSystemRequestBus::Broadcast(&AZ::ScriptSystemRequests::GarbageCollect);
-    }
-
-    void EditorScriptCanvasComponent::SetAssetId(const SourceHandle& assetId)
+    void EditorScriptCanvasComponent::SetAssetId(const AZ::Data::AssetId& assetId)
     {
         if (m_sourceHandle.Describe() != assetId.Describe())
         {
