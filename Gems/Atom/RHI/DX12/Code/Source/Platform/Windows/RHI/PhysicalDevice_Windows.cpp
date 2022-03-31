6cfe7c8a41c252b66b0df1d175c7eb3b5f46d145
byrcolin <byrcolin@amazon.com>
2022-03-30 19:51:56 -0700

    just trying some xr init code in the windows physical device
    Signed-off-by: byrcolin <byrcolin@amazon.com>

--- Gems/Atom/RHI/DX12/Code/Source/Platform/Windows/RHI/PhysicalDevice_Windows.cpp
+++ Gems/Atom/RHI/DX12/Code/Source/Platform/Windows/RHI/PhysicalDevice_Windows.cpp
@@ -9,6 +9,63 @@
 #include <RHI/PhysicalDevice_Windows.h>
 #include <AzCore/std/string/conversions.h>
 
+// Tell OpenXR what platform code we'll be using
+#define XR_USE_PLATFORM_WIN32
+#define XR_USE_GRAPHICS_API_D3D12
+#include <openxr/openxr.h>
+#include <openxr/openxr_platform.h>
+
+#include <RHI/DX12_Windows.h>
+
+//struct swapchain_surfdata_t
+//{
+//    ID3D12DepthStencilView* depth_view;
+//    ID3D12RenderTargetView* target_view;
+//};
+//
+//struct swapchain_t
+//{
+//    XrSwapchain handle;
+//    int32_t width;
+//    int32_t height;
+//    std::vector<XrSwapchainImageD3D12KHR> surface_images;
+//    std::vector<swapchain_surfdata_t> surface_data;
+//};
+
+struct input_state_t
+{
+    XrActionSet actionSet;
+    XrAction poseAction;
+    XrAction selectAction;
+    XrPath handSubactionPath[2];
+    XrSpace handSpace[2];
+    XrPosef handPose[2];
+    XrBool32 renderHand[2];
+    XrBool32 handSelect[2];
+};
+
+// Function pointers for some OpenXR extension methods we'll use.
+PFN_xrGetD3D12GraphicsRequirementsKHR ext_xrGetD3D12GraphicsRequirementsKHR = nullptr;
+PFN_xrCreateDebugUtilsMessengerEXT ext_xrCreateDebugUtilsMessengerEXT = nullptr;
+PFN_xrDestroyDebugUtilsMessengerEXT ext_xrDestroyDebugUtilsMessengerEXT = nullptr;
+
+
+const XrPosef xr_pose_identity = { { 0, 0, 0, 1 }, { 0, 0, 0 } };
+XrInstance xr_instance = {};
+XrSession xr_session = {};
+XrSessionState xr_session_state = XR_SESSION_STATE_UNKNOWN;
+bool xr_running = false;
+XrSpace xr_app_space = {};
+XrSystemId xr_system_id = XR_NULL_SYSTEM_ID;
+input_state_t xr_input = {};
+XrEnvironmentBlendMode xr_blend = {};
+XrDebugUtilsMessengerEXT xr_debug = {};
+
+std::vector<XrView> xr_views;
+std::vector<XrViewConfigurationView> xr_config_views;
+//std::vector<swapchain_t> xr_swapchains;
+
+
 namespace AZ
 {
     namespace DX12
@@ -36,6 +93,72 @@ namespace AZ
 
         void PhysicalDevice::Init(IDXGIFactoryX* factory, IDXGIAdapterX* adapter)
         {
+            // OpenXR will fail to initialize if we ask for an extension that OpenXR
+            // can't provide! So we need to check our all extensions before
+            // initializing OpenXR with them. Note that even if the extension is
+            // present, it's still possible you may not be able to use it. For
+            // example: the hand tracking extension may be present, but the hand
+            // sensor might not be plugged in or turned on. There are often
+            // additional checks that should be made before using certain features!
+            std::vector<const char*> use_extensions;
+            const char* ask_extensions[] = {
+                XR_KHR_D3D12_ENABLE_EXTENSION_NAME, // Use Direct3D12 for rendering
+                XR_EXT_DEBUG_UTILS_EXTENSION_NAME, // Debug utils for extra info
+            };
+
+            // We'll get a list of extensions that OpenXR provides using this
+            // enumerate pattern. OpenXR often uses a two-call enumeration pattern
+            // where the first call will tell you how much memory to allocate, and
+            // the second call will provide you with the actual data!
+            uint32_t ext_count = 0;
+            xrEnumerateInstanceExtensionProperties(nullptr, 0, &ext_count, nullptr);
+            std::vector<XrExtensionProperties> xr_exts(ext_count, { XR_TYPE_EXTENSION_PROPERTIES });
+            xrEnumerateInstanceExtensionProperties(nullptr, ext_count, &ext_count, xr_exts.data());
+
+            printf("OpenXR extensions available:\n");
+            for (size_t i = 0; i < xr_exts.size(); i++)
+            {
+                printf("- %s\n", xr_exts[i].extensionName);
+
+                // Check if we're asking for this extensions, and add it to our use
+                // list!
+                for (int32_t ask = 0; ask < _countof(ask_extensions); ask++)
+                {
+                    if (strcmp(ask_extensions[ask], xr_exts[i].extensionName) == 0)
+                    {
+                        use_extensions.push_back(ask_extensions[ask]);
+                        break;
+                    }
+                }
+            }
+
+            // Initialize OpenXR with the extensions we've found!
+            XrInstanceCreateInfo createInfo = { XR_TYPE_INSTANCE_CREATE_INFO };
+            createInfo.enabledExtensionCount = (uint32_t)use_extensions.size();
+            createInfo.enabledExtensionNames = use_extensions.data();
+            createInfo.applicationInfo.apiVersion = XR_CURRENT_API_VERSION;
+            strcpy_s(createInfo.applicationInfo.applicationName, "Test AppName");
+            xrCreateInstance(&createInfo, &xr_instance);
+
+            // Check if OpenXR is on this system, if this is null here, the user
+            // needs to install an OpenXR runtime and ensure it's active!
+            assert(xr_instance);
+
+            // Load extension methods that we'll need for this application! There's a
+            // couple ways to do this, and this is a fairly manual one. Chek out this
+            // file for another way to do it:
+            // https://github.com/maluoi/StereoKit/blob/master/StereoKitC/systems/platform/openxr_extensions.h
+            xrGetInstanceProcAddr(xr_instance, "xrCreateDebugUtilsMessengerEXT", (PFN_xrVoidFunction*)(&ext_xrCreateDebugUtilsMessengerEXT));
+            xrGetInstanceProcAddr(xr_instance, "xrDestroyDebugUtilsMessengerEXT", (PFN_xrVoidFunction*)(&ext_xrDestroyDebugUtilsMessengerEXT));
+            xrGetInstanceProcAddr(xr_instance, "xrGetD3D12GraphicsRequirementsKHR", (PFN_xrVoidFunction*)(&ext_xrGetD3D12GraphicsRequirementsKHR));
+
+
+
+
+
+
+
+
             m_dxgiFactory = factory;
             m_dxgiAdapter = adapter;
 
