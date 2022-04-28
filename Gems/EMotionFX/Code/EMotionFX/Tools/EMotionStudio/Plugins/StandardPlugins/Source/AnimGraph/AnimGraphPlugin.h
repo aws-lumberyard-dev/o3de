ffc028e4c5b1f5386ac2fa0c474ef867e627b1d9
John Jones-Steele <82226755+jjjoness@users.noreply.github.com>
2022-04-28 17:55:35 +0100

    Animgraph editing working for animgraph and motionset
    Signed-off-by: John Jones-Steele <82226755+jjjoness@users.noreply.github.com>

--- Gems/EMotionFX/Code/EMotionFX/Tools/EMotionStudio/Plugins/StandardPlugins/Source/AnimGraph/AnimGraphPlugin.h
+++ Gems/EMotionFX/Code/EMotionFX/Tools/EMotionStudio/Plugins/StandardPlugins/Source/AnimGraph/AnimGraphPlugin.h
@@ -237,6 +237,7 @@ namespace EMStudio
 
         void SetActionFilter(const AnimGraphActionFilter& actionFilter);
         const AnimGraphActionFilter& GetActionFilter() const;
+        void FileOpen(AZStd::string filename);
 
     private:
         enum EDockWindowOptionFlag
