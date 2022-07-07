2075904b9c04e8f0673f7095282bd4ffaff03194
srikappa-amzn <82230713+srikappa-amzn@users.noreply.github.com>
2022-07-07 15:59:51 -0700

    Mark target instance as nullptr when no focus instance is found
    Signed-off-by: srikappa-amzn <82230713+srikappa-amzn@users.noreply.github.com>

--- Code/Framework/AzToolsFramework/AzToolsFramework/Prefab/Instance/InstanceDomGenerator.cpp
+++ Code/Framework/AzToolsFramework/AzToolsFramework/Prefab/Instance/InstanceDomGenerator.cpp
@@ -54,12 +54,13 @@ namespace AzToolsFramework
         {
             // Retrieve focused instance
             auto focusedInstance = m_prefabFocusInterface->GetFocusedPrefabInstance(s_editorEntityContextId);
-            if (!focusedInstance.has_value())
+            Instance* targetInstance = nullptr;
+            if (focusedInstance.has_value())
             {
-                return false;
+                targetInstance = &(focusedInstance->get());
             }
 
-            auto climbUpToDomSourceInstanceeResult = PrefabInstanceUtils::ClimbUpToTargetInstance(instance, &focusedInstance->get());
+            auto climbUpToDomSourceInstanceeResult = PrefabInstanceUtils::ClimbUpToTargetInstance(instance, targetInstance);
             auto domSourceInstance = climbUpToDomSourceInstanceeResult.first;
             AZStd::string& relativePathToDomSourceInstance = climbUpToDomSourceInstanceeResult.second;
 
@@ -77,15 +78,14 @@ namespace AzToolsFramework
             instanceDom.CopyFrom(*instanceDomValueFromSource, instanceDom.GetAllocator());
 
             // If the focused instance is not an ancestor of our instance, verify if it's a descendant.
-            if (domSourceInstance != &focusedInstance->get())
+            if (domSourceInstance != targetInstance)
             {
-                auto climbUpToFocusedInstanceAncestorResult =
-                    PrefabInstanceUtils::ClimbUpToTargetInstance(&focusedInstance->get(), instance);
+                auto climbUpToFocusedInstanceAncestorResult = PrefabInstanceUtils::ClimbUpToTargetInstance(targetInstance, instance);
                 auto focusedInstanceAncestor = climbUpToFocusedInstanceAncestorResult.first;
                 AZStd::string& relativePathToFocusedInstanceAncestor = climbUpToFocusedInstanceAncestorResult.second;
 
                 // If the focused instance is a descendant (or the instance itself), we need to replace its portion of the dom with the template one.
-                if (focusedInstanceAncestor == instance)
+                if (focusedInstanceAncestor != nullptr && focusedInstanceAncestor == instance)
                 {
                     // Get the dom for the focused instance from its template.
                     PrefabDom focusedInstanceDom;
@@ -109,7 +109,7 @@ namespace AzToolsFramework
                 }
             }
             // If our instance is the focused instance, fix the container
-            else if (&focusedInstance->get() == instance)
+            else if (targetInstance == instance)
             {
                 // Replace the container entity with the one as seen by the root
                 // TODO - this function should only replace the transform!
