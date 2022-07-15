a8035e629f75b8b9521528ad937451888eb95706
Judy Ng <njud@amazon.com>
2022-07-15 13:14:43 -0700

    Add all the template and final files
    Signed-off-by: Judy Ng <njud@amazon.com>

---
+++ Gems/Atom/Feature/Common/SixPointLightingTutorial/FinalFiles/SixPointLighting_TexturePackEnum.lua
@@ -0,0 +1,36 @@
+--------------------------------------------------------------------------------------
+--
+-- Copyright (c) Contributors to the Open 3D Engine Project.
+-- For complete copyright and license terms please see the LICENSE at the root of this distribution.
+--
+-- SPDX-License-Identifier: Apache-2.0 OR MIT
+--
+--
+--
+----------------------------------------------------------------------------------------------------
+
+TexturePackMode_TpLftRtBt_FrBck = 0;
+TexturePackMode_LftRtTp_FrBckBt = 1;
+
+function GetMaterialPropertyDependencies()
+    return {"sixPointLighting.texturePackMode"}
+end
+
+function ProcessEditor(context)
+    local texturePackMode = context:GetMaterialPropertyValue_enum("sixPointLighting.texturePackMode");
+
+    -- Depending on which texture pack mode is being used, display the correct one
+    if(texturePackMode == TexturePackMode_TpLftRtBt_FrBck) then
+        -- TpLftRtBt is the first texture, FrBck is the second. Disable LftRtTp and FrBckBt.
+        context:SetMaterialPropertyVisibility("sixPointLighting.TLRB", MaterialPropertyVisibility_Enabled)
+        context:SetMaterialPropertyVisibility("sixPointLighting.FB", MaterialPropertyVisibility_Enabled)
+        context:SetMaterialPropertyVisibility("sixPointLighting.LRT", MaterialPropertyVisibility_Hidden)
+        context:SetMaterialPropertyVisibility("sixPointLighting.FBB", MaterialPropertyVisibility_Hidden)
+    elseif(texturePackMode == TexturePackMode_LftRtTp_FrBckBt) then
+        -- LftRtTp is the first texture, FrBckBt is the second. Disable TpLftRtBt and FrBck.
+        context:SetMaterialPropertyVisibility("sixPointLighting.TLRB", MaterialPropertyVisibility_Hidden)
+        context:SetMaterialPropertyVisibility("sixPointLighting.FB", MaterialPropertyVisibility_Hidden)
+        context:SetMaterialPropertyVisibility("sixPointLighting.LRT", MaterialPropertyVisibility_Enabled)
+        context:SetMaterialPropertyVisibility("sixPointLighting.FBB", MaterialPropertyVisibility_Enabled)
+    end
+end
