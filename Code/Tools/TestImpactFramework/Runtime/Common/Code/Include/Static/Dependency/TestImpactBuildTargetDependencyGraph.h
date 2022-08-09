cbe5abbda3b31ecd80cba20eb2ee123bc2baf593
John <jonawals@amazon.com>
2022-08-09 19:44:34 +0100

    WIP: dependency graph.
    Signed-off-by: John <jonawals@amazon.com>

--- Code/Tools/TestImpactFramework/Runtime/Common/Code/Include/Static/Dependency/TestImpactBuildTargetDependencyGraph.h
+++ Code/Tools/TestImpactFramework/Runtime/Common/Code/Include/Static/Dependency/TestImpactBuildTargetDependencyGraph.h
@@ -14,22 +14,73 @@
 
 namespace TestImpact
 {
+    //!
+    template<typename ProductionTarget, typename TestTarget>
+    struct BuildTargetDependencyGraphNode;
+
+    //!
+    template<typename ProductionTarget, typename TestTarget>
+    using BuildTargetDependencyList = AZStd::vector<BuildTargetDependencyGraphNode<ProductionTarget*, TestTarget>>;
+
+    //!
+    template<typename ProductionTarget, typename TestTarget>
+    struct BuildTargetDependencies
+    {
+        BuildTargetDependencyList<ProductionTarget, TestTarget> m_build; //!<
+        BuildTargetDependencyList<ProductionTarget, TestTarget> m_runtime; //!<
+    };
+
+    //!
+    template<typename ProductionTarget, typename TestTarget>
+    struct BuildTargetDependencyGraphNode
+    {
+        BuildTarget<ProductionTarget, TestTarget> m_buildTarget; //!<
+        BuildTargetDependencies<ProductionTarget, TestTarget> m_dependencies; //!<
+    };
+
+    //!
     template<typename ProductionTarget, typename TestTarget>
     class BuildTargetDependencyGraph
     {
     public:
-        struct BuildTargetDependencyList
-        {
-        };
 
-        BuildTargetDependencyGraph(const BuildTargetList<ProductionTarget, TestTarget>* buildTargetList);
+        //!
+        BuildTargetDependencyGraph(const BuildTargetList<ProductionTarget, TestTarget>& buildTargetList);
+
+        //!
+        //const BuildTargetDependencies<ProductionTarget, TestTarget> GetBuildTargetDependencies(
+        //    const BuildTarget<ProductionTarget, TestTarget>& buildTarget) const;
 
     private:
+        AZStd::unordered_map<BuildTarget<ProductionTarget, TestTarget>, BuildTargetDependencyGraphNode<ProductionTarget, TestTarget>>
+            m_buildTargetDependencyMap;
     };
 
     template<typename ProductionTarget, typename TestTarget>
     BuildTargetDependencyGraph<ProductionTarget, TestTarget>::BuildTargetDependencyGraph(
-        const BuildTargetList<ProductionTarget, TestTarget>* buildTargets)
+        const BuildTargetList<ProductionTarget, TestTarget>& buildTargetList)
     {
+        for (const auto& buildTarget : buildTargetList.GetBuildTargets())
+        {
+            auto& node = m_buildTargetDependencyMap[buildTarget];
+            for (const auto& buildDependency : buildTarget.GetTarget().GetDependencies().m_build)
+            {
+
+            }
+        }
     }
 } // namespace TestImpact
+
+//namespace AZStd
+//{
+//    //! Hash function for BuildTarget types for use in maps and sets.
+//    template<typename ProductionTarget, typename TestTarget>
+//    struct hash<TestImpact::BuildTargetDependencyGraphNode<ProductionTarget, TestTarget>>
+//    {
+//        size_t operator()(
+//            const TestImpact::BuildTargetDependencyGraphNode<ProductionTarget, TestTarget>& node) const noexcept
+//        {
+//            return reinterpret_cast<size_t>(node.m_buildTarget.GetTarget());
+//        }
+//    };
+//} // namespace AZStd
