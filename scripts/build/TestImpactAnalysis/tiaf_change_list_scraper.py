712c02dcc03b02b2d207661da9771a68a6339a9f
Jack Curtis <jcurtisk@amazon.com>
2022-08-12 14:10:21 +0100

    Add extension object and some printing to scraper
    Signed-off-by: Jack Curtis <jcurtisk@amazon.com>

--- scripts/build/TestImpactAnalysis/tiaf_change_list_scraper.py
+++ scripts/build/TestImpactAnalysis/tiaf_change_list_scraper.py
@@ -1,10 +1,27 @@
-from unittest import skip
+
 import git
 import json
+from collections import namedtuple
+import math
 DEBUG = False
 
-def main():
-    x = 10
+def main(include_filters: list):
+
+    files_by_extension = {}
+    total_changes = 0
+
+    def diff_list_to_str_paths(changelist):
+        paths = []
+        for change in changelist:
+            file_name = change.a_path
+            file_extension = "." + file_name.split(".")[-1]
+            files_for_extension = files_by_extension.get(file_extension, [])
+            files_for_extension.append(file_name)
+            files_by_extension[file_extension] = files_for_extension
+            paths.append(f"{change.a_path}")
+        return paths
+
+    x = 100
     pr_commits = {}
     with git.Repo("C:/o3de") as repo:
         head = repo.head.commit
@@ -13,15 +30,48 @@ def main():
             if parent_count > 1:
                 if DEBUG: print(head.summary)
                 diff = head.diff(head.parents[0])
-                pr_commits[str(head)] = diff
+                change_paths_for_this_commit = diff_list_to_str_paths(diff)
+                total_changes = total_changes + len(change_paths_for_this_commit)
+                pr_commits[str(head)] = change_paths_for_this_commit
             head = head.parents[0]
-            
-    print(len(pr_commits))
-    with open("jsonchangelists.json", "w+") as f:
-        json.dump(pr_commits, f, skipkeys=True)
 
+    extension_statistics = {}
+    
+    for file, entry in files_by_extension.items():
+        count = len(entry)
+        percentage = count/total_changes * 100
+        statistics_obj = ExtensionStatistics(file_extension=file, count=count, percentage=percentage)
+        extension_statistics[file] = statistics_obj
+    
+    for extension in include_filters:
+        print(extension_statistics.get(extension, f"The extension {extension} did not appear in this set of changelists."))
 
+class ExtensionStatistics():
+    
+    def __init__(self, *args, **kwargs):
+        self._count = kwargs.pop('count')
+        self._percentage = kwargs.pop('percentage')
+        self._file_ext = kwargs.pop('file_extension')
 
+    @property
+    def percentage(self):
+        return round(self._percentage, 2)
+
+    @property
+    def percentage_raw(self):
+        return self._percentage
+
+    @property
+    def file_ext(self):
+        return self._file_ext
+
+    @property
+    def count(self):
+        return self._count
+
+    def __str__(self):
+        return f"{self.file_ext} occurred {self.count} times, and made up {self.percentage}% of total file changes."
 
 if __name__ == "__main__":
-    main()
+    include_filters = [".h", ".hpp", ".hxx", ".inl", ".c", ".cpp", ".cxx", ".py"]
+    main(include_filters)
