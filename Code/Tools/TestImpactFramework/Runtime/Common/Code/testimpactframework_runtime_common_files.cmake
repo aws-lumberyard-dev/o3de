629dd57ba261a53b9cf28a56de99891591fced01
John <jonawals@amazon.com>
2022-08-15 15:34:02 +0100

    Merge
    Signed-off-by: John <jonawals@amazon.com>

--- Code/Tools/TestImpactFramework/Runtime/Common/Code/testimpactframework_runtime_common_files.cmake
+++ Code/Tools/TestImpactFramework/Runtime/Common/Code/testimpactframework_runtime_common_files.cmake
@@ -70,6 +70,8 @@ set(FILES
     Include/Static/TestImpactRuntimeUtils.cpp
     Include/Static/TestImpactRuntimeUtils.h
     Include/Static/TestImpactTestTargetExclusionList.h
+    Include/Static/TestImpactRuntimeUtils.cpp
+    Include/Static/TestImpactRuntimeUtils.h
     Source/Artifact/Factory/TestImpactTestEnumerationSuiteFactory.cpp
     Source/Artifact/Factory/TestImpactTestRunSuiteFactory.cpp
     Source/Artifact/Factory/TestImpactModuleCoverageFactory.cpp
