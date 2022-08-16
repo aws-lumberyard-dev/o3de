e889e827a0d7b0b084150a1ce65e343ce8d01f01
Jack Curtis <jcurtisk@amazon.com>
2022-08-16 11:23:05 +0100

    Working version of path analysis with some gaps
    Signed-off-by: Jack Curtis <jcurtisk@amazon.com>

--- scripts/build/TestImpactAnalysis/change_list_scraper/tiaf_change_list_scraper.py
+++ scripts/build/TestImpactAnalysis/change_list_scraper/tiaf_change_list_scraper.py
@@ -67,12 +67,78 @@ def parse_args():
         required=False
     )
 
+    parser.add_argument(
+        "--path_analysis",
+        action="store_true",
+        help="Should path analysis be used in this run.",
+        required=False
+    )
+
     args = parser.parse_args()
 
     return args
 
 
 RAW_PATH_FILE = "build/commits_raw.json"
+
+def analyse_associations_by_changelist(change_list_map):
+    analysis_map = {}
+    for pr, map in change_list_map.items():
+        common_paths = {}
+        del map['commit']
+        purepaths = []
+        for change_list in map.values():
+            for path in change_list:
+                purepaths.append(pathlib.PurePath(path))
+                # Sort alphabetically, go from root deeper into structure. Stop when you find the first directory that is different.
+        purepaths = sorted(purepaths)
+        
+        for i in range(0, len(purepaths)-1):
+            first_file = purepaths[i]
+            second_file = purepaths[i+1]
+            previous_path = pathlib.Path('.')
+            depth = 0
+            for first_path, second_path in zip(first_file.parts, second_file.parts):
+                if not first_path == second_path:
+                    if previous_path:
+                        associated_paths = common_paths.get(previous_path,set())
+                        associated_paths.add(first_file)
+                        associated_paths.add(second_file)
+                        common_paths[previous_path] = associated_paths
+                        break
+                previous_path = pathlib.Path.joinpath(previous_path, first_path)
+                depth += 1
+        analysis_map[pr] = common_paths
+    return analysis_map
+
+def analyse_associations_holistically(change_list_map):
+    purepaths = []
+    common_paths = {}
+    for pr, map in change_list_map.items():
+        del map['commit']
+        
+        for change_list in map.values():
+            for path in change_list:
+                purepaths.append(pathlib.PurePath(path))
+                # Sort alphabetically, go from root deeper into structure. Stop when you find the first directory that is different.
+    purepaths = sorted(purepaths)
+        
+    for i in range(0, len(purepaths)-1):
+        first_file = purepaths[i]
+        second_file = purepaths[i+1]
+        previous_path = pathlib.Path('.')
+        depth = 0
+        for first_path, second_path in zip(first_file.parts, second_file.parts):
+            if not first_path == second_path:
+                if previous_path:
+                    associated_paths = common_paths.get(previous_path,set())
+                    associated_paths.add(first_file)
+                    associated_paths.add(second_file)
+                    common_paths[previous_path] = associated_paths
+                    break
+            previous_path = pathlib.Path.joinpath(previous_path, first_path)
+            depth += 1
+    return common_paths
 
 
 def main(include_filters=[], get_from_git=False, repo_path=None, prs_to_get=None, csv_file_path=None, change_lists_folder=None, path_analysis=None):
@@ -96,13 +162,19 @@ def main(include_filters=[], get_from_git=False, repo_path=None, prs_to_get=None
         statistics_obj = ExtensionStatistics(
             file_extension=file, count=count, percentage=percentage)
         extension_statistics[file] = statistics_obj
-
     if(change_lists_folder):
         change_list_map = scraper.extract_change_lists(pr_commits)
         DataWriter.dump_tiaf_compatible_change_lists_to_json(
             change_list_map, change_lists_folder)
+
     if(path_analysis):
-        pass
+        PER_CHANGE_LIST = True
+        change_list_map = scraper.extract_change_lists(pr_commits)
+        if(PER_CHANGE_LIST):
+            analysis_map = analyse_associations_by_changelist(change_list_map)
+        else:
+            analysis_map = analyse_associations_holistically(change_list_map)
+
     if(DEBUG):
         DataWriter.print_extension_statistics(
             extension_statistics, include_filters)
