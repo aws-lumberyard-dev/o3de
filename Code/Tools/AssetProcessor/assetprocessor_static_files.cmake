359e24c4102a790d763318cfb7ebc3ad4f97e5fc
amzn-mike <80125227+amzn-mike@users.noreply.github.com>
2022-08-22 15:02:37 -0500

    Updated the majority of AP code to use SourceAssetReference
    Signed-off-by: amzn-mike <80125227+amzn-mike@users.noreply.github.com>

--- Code/Tools/AssetProcessor/assetprocessor_static_files.cmake
+++ Code/Tools/AssetProcessor/assetprocessor_static_files.cmake
@@ -36,6 +36,8 @@ set(FILES
     native/AssetManager/ExcludedFolderCache.cpp
     native/AssetManager/ExcludedFolderCache.h
     native/AssetManager/ExcludedFolderCacheInterface.h
+    native/AssetManager/SourceAssetReference.h
+    native/AssetManager/SourceAssetReference.cpp
     native/assetprocessor.h
     native/connection/connection.cpp
     native/connection/connection.h
@@ -110,6 +112,7 @@ set(FILES
     native/utilities/ThreadHelper.h
     native/utilities/UuidManager.h
     native/utilities/UuidManager.cpp
+    native/utilities/IPathConversion.h
 )
 
 set(SKIP_UNITY_BUILD_INCLUSION_FILES
