cdbc2824f2fe1eef1c2ee4819d06c00853be2e93
Jack Curtis <jcurtisk@amazon.com>
2022-08-23 14:30:08 +0100

    Add generator for test enumeration job info
    Signed-off-by: Jack Curtis <jcurtisk@amazon.com>

--- Code/Tools/TestImpactFramework/Runtime/Python/Code/Source/TestRunner/Python/Job/TestImpactPythonTestJobInfoGenerator.cpp
+++ Code/Tools/TestImpactFramework/Runtime/Python/Code/Source/TestRunner/Python/Job/TestImpactPythonTestJobInfoGenerator.cpp
@@ -13,6 +13,7 @@
 
 namespace TestImpact
 {
+
     PythonTestRunJobInfoGenerator::PythonTestRunJobInfoGenerator(
         const RepoPath& repoDir, const RepoPath& buildDir, const ArtifactDir& artifactDir)
         : m_repoDir(repoDir)
@@ -40,4 +41,43 @@ namespace TestImpact
 
         return JobInfo(jobId, args, JobData(runArtifact, m_artifactDir.m_coverageArtifactDirectory / parentFolderName));
     }
+
+    PythonTestEnumerationJobInfoGenerator::PythonTestEnumerationJobInfoGenerator(
+        const RepoPath& buildDir, const RepoPath& cacheDir, const ArtifactDir& artifactDir, const RepoPath& pythonDir)
+        : m_buildDir(buildDir)
+        , m_cacheDir(cacheDir)
+        , m_artifactDir(artifactDir)
+        , m_pythonCommand(pythonDir)  
+    {
+    }
+
+    void PythonTestEnumerationJobInfoGenerator::SetCachePolicy(PythonTestEnumerator::JobInfo::CachePolicy cachePolicy)
+    {
+        m_cachePolicy = cachePolicy;
+    }
+
+    PythonTestEnumerator::JobInfo::CachePolicy PythonTestEnumerationJobInfoGenerator::GetCachePolicy() const
+    {
+        return m_cachePolicy;
+    }
+
+    AZStd::string PythonTestEnumerationJobInfoGenerator::CompileBuildDirectoryArgument() const
+    {
+        return AZStd::string::format("--build-directory=%s ", m_buildDir.c_str());
+    }
+
+    PythonTestEnumerator::JobInfo PythonTestEnumerationJobInfoGenerator::GenerateJobInfo(
+        const PythonTestTarget* testTarget, PythonTestEnumerator::JobInfo::Id jobId) const
+    {
+        using Cache = PythonTestEnumerator::JobData::Cache;
+
+        const auto enumerationArtifact = GenerateTargetEnumerationArtifactFilePath(testTarget, m_artifactDir.m_enumerationArtifactDirectory);
+        const AZStd::string pytestAndFixedArgs = " -m pytest --collect-only --continue-on-collection-errors -q ";
+        const AZStd::string buildDirectory = CompileBuildDirectoryArgument();
+        const AZStd::string scriptPath = testTarget->GetScriptPath().String();
+        const Command args = { m_pythonCommand.String() + pytestAndFixedArgs + buildDirectory + scriptPath + " >" + enumerationArtifact.String()};
+
+        return JobInfo(jobId, args, JobData(enumerationArtifact, Cache{ m_cachePolicy, GenerateTargetEnumerationCacheFilePath(testTarget, m_cacheDir)}));
+    }
+
 } // namespace TestImpact
--- Code/Tools/TestImpactFramework/Runtime/Python/Code/Source/TestRunner/Python/Job/TestImpactPythonTestJobInfoGenerator.h
+++ Code/Tools/TestImpactFramework/Runtime/Python/Code/Source/TestRunner/Python/Job/TestImpactPythonTestJobInfoGenerator.h
@@ -13,6 +13,7 @@
 #include <Target/Python/TestImpactPythonTestTarget.h>
 #include <TestRunner/Common/Job/TestImpactTestJobInfoGenerator.h>
 #include <TestRunner/Python/TestImpactPythonTestRunner.h>
+#include <TestRunner/Python/TestImpactPythonTestEnumerator.h>
 
 namespace TestImpact
 {
@@ -37,5 +38,36 @@ namespace TestImpact
         RepoPath m_repoDir;
         RepoPath m_buildDir;
         ArtifactDir m_artifactDir;
+    };
+
+    class PythonTestEnumerationJobInfoGenerator
+        : public TestJobInfoGenerator<PythonTestEnumerator, PythonTestTarget>
+    {
+    public:
+        //! Configures the test job info generator with the necessary path information for launching test targets.
+        //! @param repoDir Root path to where the repository is located.
+        //! @param buildDir Path to where the target binaries are found.
+        //! @param artifactDir Path to the transient directory where test artifacts are produced.
+        PythonTestEnumerationJobInfoGenerator(
+            const RepoPath& buildDir, const RepoPath& cacheDir, const ArtifactDir& artifactDir, const RepoPath& pythonDir);
+
+        //! Generates the information for a test run job.
+        //! @param testTarget The test target to generate the job information for.
+        //! @param jobId The id to assign for this job.
+        PythonTestEnumerator::JobInfo GenerateJobInfo(const PythonTestTarget* testTarget, PythonTestEnumerator::JobInfo::Id jobId) const;
+
+        AZStd::string CompileBuildDirectoryArgument() const;
+
+        void SetCachePolicy(PythonTestEnumerator::JobInfo::CachePolicy cachePolicy);
+
+        //!
+        PythonTestEnumerator::JobInfo::CachePolicy GetCachePolicy() const;
+    private:
+        RepoPath m_buildDir;
+        RepoPath m_cacheDir;
+        ArtifactDir m_artifactDir;
+        RepoPath m_pythonCommand;
+
+        PythonTestEnumerator::JobInfo::CachePolicy m_cachePolicy = PythonTestEnumerator::JobInfo::CachePolicy::Write;
     };
 } // namespace TestImpact
