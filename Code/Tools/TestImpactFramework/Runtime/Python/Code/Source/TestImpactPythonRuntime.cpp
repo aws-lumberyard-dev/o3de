b5ad35f31ddde92a7db6b2c99b672e47ab183245
John <jonawals@amazon.com>
2022-11-21 14:53:44 +0000

    PR comments.
    Signed-off-by: John <jonawals@amazon.com>

--- Code/Tools/TestImpactFramework/Runtime/Python/Code/Source/TestImpactPythonRuntime.cpp
+++ Code/Tools/TestImpactFramework/Runtime/Python/Code/Source/TestImpactPythonRuntime.cpp
@@ -617,14 +617,14 @@ TestEngineInstrumentedRunResult<PythonTestTarget, TestCoverage> DiscoverDependen
     }
 
     Client::ImpactAnalysisSequenceReport PythonRuntime::ImpactAnalysisTestSequence(
-        [[maybe_unused]] const ChangeList& changeList,
-        [[maybe_unused]] Policy::TestPrioritization testPrioritizationPolicy,
-        [[maybe_unused]] Policy::DynamicDependencyMap dynamicDependencyMapPolicy,
-        [[maybe_unused]] AZStd::optional<AZStd::chrono::milliseconds> testTargetTimeout,
-        [[maybe_unused]] AZStd::optional<AZStd::chrono::milliseconds> globalTimeout,
-        [[maybe_unused]] AZStd::optional<ImpactAnalysisTestSequenceStartCallback> testSequenceStartCallback,
-        [[maybe_unused]] AZStd::optional<TestSequenceCompleteCallback<Client::ImpactAnalysisSequenceReport>> testSequenceEndCallback,
-        [[maybe_unused]] AZStd::optional<TestRunCompleteCallback> testCompleteCallback)
+        const ChangeList& changeList,
+        Policy::TestPrioritization testPrioritizationPolicy,
+        Policy::DynamicDependencyMap dynamicDependencyMapPolicy,
+        AZStd::optional<AZStd::chrono::milliseconds> testTargetTimeout,
+        AZStd::optional<AZStd::chrono::milliseconds> globalTimeout,
+        AZStd::optional<ImpactAnalysisTestSequenceStartCallback> testSequenceStartCallback,
+        AZStd::optional<TestSequenceCompleteCallback<Client::ImpactAnalysisSequenceReport>> testSequenceEndCallback,
+        AZStd::optional<TestRunCompleteCallback> testCompleteCallback)
     {
         const Timer sequenceTimer;
 
@@ -722,13 +722,13 @@ TestEngineInstrumentedRunResult<PythonTestTarget, TestCoverage> DiscoverDependen
     }
 
     Client::SafeImpactAnalysisSequenceReport PythonRuntime::SafeImpactAnalysisTestSequence(
-        [[maybe_unused]] const ChangeList& changeList,
-        [[maybe_unused]] Policy::TestPrioritization testPrioritizationPolicy,
-        [[maybe_unused]] AZStd::optional<AZStd::chrono::milliseconds> testTargetTimeout,
-        [[maybe_unused]] AZStd::optional<AZStd::chrono::milliseconds> globalTimeout,
-        [[maybe_unused]] AZStd::optional<SafeImpactAnalysisTestSequenceStartCallback> testSequenceStartCallback,
-        [[maybe_unused]] AZStd::optional<TestSequenceCompleteCallback<Client::SafeImpactAnalysisSequenceReport>> testSequenceEndCallback,
-        [[maybe_unused]] AZStd::optional<TestRunCompleteCallback> testCompleteCallback)
+        const ChangeList& changeList,
+        Policy::TestPrioritization testPrioritizationPolicy,
+        AZStd::optional<AZStd::chrono::milliseconds> testTargetTimeout,
+        AZStd::optional<AZStd::chrono::milliseconds> globalTimeout,
+        AZStd::optional<SafeImpactAnalysisTestSequenceStartCallback> testSequenceStartCallback,
+        AZStd::optional<TestSequenceCompleteCallback<Client::SafeImpactAnalysisSequenceReport>> testSequenceEndCallback,
+        AZStd::optional<TestRunCompleteCallback> testCompleteCallback)
     {
         const Timer sequenceTimer;
         TestRunData<TestEngineInstrumentedRun<TestTarget, TestCoverage>> selectedTestRunData, draftedTestRunData;
@@ -841,6 +841,24 @@ TestEngineInstrumentedRunResult<PythonTestTarget, TestCoverage> DiscoverDependen
             gatherTestRunData(draftedTestTargets, instrumentedTestRun, draftedTestRunData);
         }
 
+        auto selectedTestRunReport = GenerateTestRunReport(
+            selectedTestRunData.m_result,
+            selectedTestRunData.m_relativeStartTime,
+            selectedTestRunData.m_duration,
+            selectedTestRunData.m_jobs);
+
+        auto discardedTestRunReport = GenerateTestRunReport(
+            discardedTestRunData.m_result,
+            discardedTestRunData.m_relativeStartTime,
+            discardedTestRunData.m_duration,
+            discardedTestRunData.m_jobs);
+
+        auto draftedTestRunReport = GenerateTestRunReport(
+            draftedTestRunData.m_result,
+            draftedTestRunData.m_relativeStartTime,
+            draftedTestRunData.m_duration,
+            draftedTestRunData.m_jobs);
+
         // Generate the sequence report for the client
         const auto sequenceReport = Client::SafeImpactAnalysisSequenceReport(
             1,
@@ -851,21 +869,9 @@ TestEngineInstrumentedRunResult<PythonTestTarget, TestCoverage> DiscoverDependen
             selectedTests,
             discardedTests,
             draftedTests,
-            GenerateTestRunReport(
-                selectedTestRunData.m_result,
-                selectedTestRunData.m_relativeStartTime,
-                selectedTestRunData.m_duration,
-                selectedTestRunData.m_jobs),
-            GenerateTestRunReport(
-                discardedTestRunData.m_result,
-                discardedTestRunData.m_relativeStartTime,
-                discardedTestRunData.m_duration,
-                discardedTestRunData.m_jobs),
-            GenerateTestRunReport(
-                draftedTestRunData.m_result,
-                draftedTestRunData.m_relativeStartTime,
-                draftedTestRunData.m_duration,
-                draftedTestRunData.m_jobs));
+            std::move(selectedTestRunReport),
+            std::move(discardedTestRunReport),
+            std::move(draftedTestRunReport));
 
         // Inform the client that the sequence has ended
         if (testSequenceEndCallback.has_value())
@@ -874,13 +880,13 @@ TestEngineInstrumentedRunResult<PythonTestTarget, TestCoverage> DiscoverDependen
         }
 
         m_hasImpactAnalysisData = UpdateAndSerializeDynamicDependencyMap(
-                                      *m_dynamicDependencyMap.get(),
-                                      ConcatenateVectors(selectedTestRunData.m_jobs, draftedTestRunData.m_jobs),
-                                      m_failedTestCoveragePolicy,
-                                      m_integrationFailurePolicy,
-                                      m_config.m_commonConfig.m_repo.m_root,
-                                      m_sparTiaFile)
-                                      .value_or(m_hasImpactAnalysisData);
+            *m_dynamicDependencyMap.get(),
+            ConcatenateVectors(selectedTestRunData.m_jobs, draftedTestRunData.m_jobs),
+            m_failedTestCoveragePolicy,
+            m_integrationFailurePolicy,
+            m_config.m_commonConfig.m_repo.m_root,
+            m_sparTiaFile)
+            .value_or(m_hasImpactAnalysisData);
 
         return sequenceReport;
     }
