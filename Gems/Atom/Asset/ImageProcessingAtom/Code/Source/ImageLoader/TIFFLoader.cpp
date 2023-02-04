3420748e283640eb6ef80dc66b58507ebc783e4a
byrcolin <byrcolin@amazon.com>
2023-02-03 18:57:25 -0800

    fix release compiler errors for unused vars
    Signed-off-by: byrcolin <byrcolin@amazon.com>

--- Gems/Atom/Asset/ImageProcessingAtom/Code/Source/ImageLoader/TIFFLoader.cpp
+++ Gems/Atom/Asset/ImageProcessingAtom/Code/Source/ImageLoader/TIFFLoader.cpp
@@ -22,7 +22,9 @@ namespace ImageProcessingAtom
 {
     namespace TIFFLoader
     {
+#if !defined(_RELEASE)
         static constexpr int TiffMaxMessageSize = 1024;
+#endif
 
 #ifdef AZ_ENABLE_TRACING
         // Note: the fatal errors are processed in LoadImageFromTIFF function.
