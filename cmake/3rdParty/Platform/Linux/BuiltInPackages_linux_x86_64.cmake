4dadb546f1c5702702a6eb1549fa91907d3a616c
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2023-05-05 16:07:00 -0700

    Update AWS Gamelift packages and hashes for linux x86 and arm64
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- cmake/3rdParty/Platform/Linux/BuiltInPackages_linux_x86_64.cmake
+++ cmake/3rdParty/Platform/Linux/BuiltInPackages_linux_x86_64.cmake
@@ -17,10 +17,9 @@ ly_associate_package(PACKAGE_NAME xxhash-0.7.4-rev1-multiplatform               
 ly_associate_package(PACKAGE_NAME cityhash-1.1-multiplatform                        TARGETS cityhash                    PACKAGE_HASH 0ace9e6f0b2438c5837510032d2d4109125845c0efd7d807f4561ec905512dd2)
 
 # platform-specific:
-ly_associate_package(PACKAGE_NAME AWSGameLiftServerSDK-5.0.0-rev1-linux             TARGETS AWSGameLiftServerSDK        PACKAGE_HASH cc4460b6bb3db12c9f922a48d6a1fd40605909805cb5691700de9e40e6772c26)
-
 ly_associate_package(PACKAGE_NAME expat-2.4.2-rev2-linux                            TARGETS expat                       PACKAGE_HASH 755369a919e744b9b3f835d1acc684f02e43987832ad4a1c0b6bbf884e6cd45b)
 ly_associate_package(PACKAGE_NAME assimp-5.2.5-rev1-linux                           TARGETS assimplib                   PACKAGE_HASH 67bd3625078b63b40ae397ef7a3e589a6f77e95d3318c97dd7075e3e22a638cd)
+ly_associate_package(PACKAGE_NAME AWSGameLiftServerSDK-5.0.0-rev1-linux             TARGETS AWSGameLiftServerSDK        PACKAGE_HASH cc4460b6bb3db12c9f922a48d6a1fd40605909805cb5691700de9e40e6772c26)
 ly_associate_package(PACKAGE_NAME tiff-4.2.0.15-rev3-linux                          TARGETS TIFF                        PACKAGE_HASH 2377f48b2ebc2d1628d9f65186c881544c92891312abe478a20d10b85877409a)
 ly_associate_package(PACKAGE_NAME freetype-2.11.1-rev1-linux                        TARGETS Freetype                    PACKAGE_HASH 28bbb850590507eff85154604787881ead6780e6eeee9e71ed09cd1d48d85983)
 ly_associate_package(PACKAGE_NAME Lua-5.4.4-rev1-linux                              TARGETS Lua                         PACKAGE_HASH d582362c3ef90e1ef175a874abda2265839ffc2e40778fa293f10b443b4697ac)
