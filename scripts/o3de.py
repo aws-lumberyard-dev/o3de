64d331322a55c6858a7b48d94d010d8836dcc72f
alexpete <alexpete@amazon.com>
2023-05-18 13:44:46 -0700

    WIP repo_properties to add gems/projects/templates
    Example usage: scripts/o3de.bat edit-repo-properties -rp d:/git/o3de-extras/repo.json --add-gems d:/git/o3de-extras/Gems/ProteusRobot --release-archive-path d:/git/o3de-extras/releases --download-prefix https://github.com/o3de/o3de-extras/releases/download/2.0
    
    Signed-off-by: Alex Peterson <26804013+AMZN-alexpete@users.noreply.github.com>

--- scripts/o3de.py
+++ scripts/o3de.py
@@ -35,7 +35,7 @@ def add_args(parser: argparse.ArgumentParser) -> None:
     from o3de import engine_properties, engine_template, gem_properties, \
         global_project, register, print_registration, get_registration, \
         enable_gem, disable_gem, project_properties, sha256, download, \
-        export_project, repo
+        export_project, repo, repo_properties
     # Remove the temporarily added path
     sys.path = sys.path[1:]
 
@@ -80,6 +80,9 @@ def add_args(parser: argparse.ArgumentParser) -> None:
     
     # repo
     repo.add_args(subparsers)
+
+    # repo properties
+    repo_properties.add_args(subparsers)
 
 
 if __name__ == "__main__":
