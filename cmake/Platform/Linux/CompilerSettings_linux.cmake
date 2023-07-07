ee672f655ccac4fe8713b633711381b5b5fc66fc
Mike Chang <changml@amazon.com>
2023-07-07 14:52:12 -0700

    Revert snap detection changes for clang
    Signed-off-by: Mike Chang <changml@amazon.com>

--- cmake/Platform/Linux/CompilerSettings_linux.cmake
+++ cmake/Platform/Linux/CompilerSettings_linux.cmake
@@ -7,24 +7,14 @@
 #
 
 if(NOT CMAKE_C_COMPILER AND NOT CMAKE_CXX_COMPILER AND NOT "$ENV{CC}" AND NOT "$ENV{CXX}")
-    if ($ENV{O3DE_SNAP})
-        set(path_search
-            $ENV{SNAP}/bin
-            $ENV{SNAP}/usr/bin
-            $ENV{SNAP}/usr/local/bin
-            $ENV{SNAP}/sbin
-            $ENV{SNAP}/usr/sbin
-            $ENV{SNAP}/usr/local/sbin
-        )
-    else()
-        set(path_search
-            /bin
-            /usr/bin
-            /usr/local/bin
-            /sbin
-            /usr/sbin
-            /usr/local/sbin
-        )
+    set(path_search
+        /bin
+        /usr/bin
+        /usr/local/bin
+        /sbin
+        /usr/sbin
+        /usr/local/sbin
+    )
 
     endif()
     list(TRANSFORM path_search APPEND "/clang-[0-9]*" OUTPUT_VARIABLE path_with_version_search)
